86c36d5ef9f4182c4f6cda14acfe38ce1bf6dd7f
Bas Veeling <b.veeling@nedforce.nl>
2011-02-22 15:27:58 +0100

    Geo static view (No script) + ContentBox View

--- app/controllers/geo_viewers_controller.rb
+++ app/controllers/geo_viewers_controller.rb
@@ -1,27 +1,37 @@
 # This +RESTful+ controller is used to orchestrate and control the flow of 
 # the application relating to GeoViewer objects.
 class GeoViewersController < ApplicationController
+  helper GeoHelper
   before_filter :find_geo_viewer, :only => :show
-  
   # * GET /geo_viewers/:id
   # * GET /geo_viewers/:id.xml
   def show
-    
     @nodes = @geo_viewer.nodes
-    
     @map = GMap.new("geo_viewer_#{@geo_viewer.id}")
     @map.control_init :small_map => true, :map_type => false
     if params[:location]
       res = Geokit::Geocoders::GoogleGeocoder.geocode(params[:location], :bias => Node.geocoding_bias) 
       @map.center_zoom_on_bounds_init(res.suggested_bounds.to_a)
+      @center = res.full_address
+    elsif @nodes.empty?
+      res = Geokit::Geocoders::GoogleGeocoder.geocode(@geo_viewer.map_settings[:center], :bias => Node.geocoding_bias) 
+      @map.center_zoom_on_bounds_init(res.suggested_bounds.to_a)
+      @center = res.full_address
     else
       @map.center_zoom_on_points_init(*@nodes.collect {|node| [node.lat, node.lng]})
     end
     
+    nodes_expl = [] # Array van nodes voor static view voor noscript
+    index = 0 # Teller om kleuren + labels te kiezen
     @nodes.each do |node|
+      nodes_expl << {:color => StaticMap::COLOURS[index % StaticMap::COLOURS.size], :label => StaticMap::LABELS[index % StaticMap::LABELS.size], :node => node}
       @map.overlay_init GMarker.new([node.lat, node.lng], :title => node.content.title, :max_width => 300, :info_window => render_to_string(:partial => '/shared/google_maps_popup', :locals => {:node => node}))
+      index += 1
     end
-        
+    
+    @expl = render_to_string(:partial => 'node_list', :object => nodes_expl)
+    
+    
     respond_to do |format|
       format.html # show.html.erb
     end
---
+++ lib/static_map.rb
@@ -0,0 +1,60 @@
+class StaticMap
+    
+      COLOURS      = %w(red green brown blue orange gray purple yellow)
+      LABELS       = ('A'..'Z').to_a
+      URL_TEMPLATE = "http://maps.google.com/maps/api/staticmap?%s"
+
+      def initialize(options = {})
+        @addresses = []
+        @width, @height = options.fetch(:width, 480), options.fetch(:height, 400)
+        @params = Hash.new.tap do |p|
+          p[:sensor]  = false
+          p[:size]    = "#{@width}x#{@height}"
+          p[:maptype] = options.fetch(:type, "roadmap")
+          #p[:zoom]    = options.fetch(:zoom, 2)
+          p[:center]  = options.fetch(:center, nil)
+        end
+      end
+
+      def <<(address)
+        @addresses << address
+      end
+
+      def to_url
+        params = @params.to_param
+        params << "&"
+        params << build_marker_params
+        (URL_TEMPLATE % params)
+      end
+
+      def self.for_address(address, opts = {})
+        map = self.new(opts.reverse_merge(:zoom => 10))
+        map << address
+        map.to_url
+      end
+
+      def self.for_addresses(*addresses)
+        map = self.new(addresses.extract_options!)
+        addresses.flatten.each { |a| map << a }
+        map.to_url
+      end
+
+      protected
+
+      def build_marker_params
+        params = []
+        @addresses.each_with_index do |address, index|
+          return "markers=#{to_ll @addresses.first}" if @addresses.size == 1
+          color = COLOURS[index % COLOURS.size]
+          label = LABELS[index % LABELS.size]
+          params << "markers=color:#{color}|label:#{label}|#{to_ll(address)}"
+        end
+        params.join("&")
+      end
+
+      def to_ll(address)
+        "#{address.lat},#{address.lng}"
+      end
+
+    end
+    
