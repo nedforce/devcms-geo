6b8c476a7b8455b628ab9837f99b61c93618f257
Roel van der Hoorn <vanderhoorn@gmail.com>
2011-10-13 16:54:50 +0200

    Refactor the geo viewer model somewhat.

--- app/models/geo_viewer.rb
+++ app/models/geo_viewer.rb
@@ -14,11 +14,11 @@
 #
 # * Requires the presence of +title+.
 #
-# * Map settings *
+# *Map settings*
 # The following map configuration options are available:
 # * default_bounding_box - The default area to show
 #
-# * Filter settings *
+# *Filter settings*
 # Specifies what default values are used for filtering and which filter options can be modified in the frontend
 # The following filter configuration options are available:
 # * Category - Filter on associated category
@@ -26,53 +26,53 @@
 # * Timeframe - Filter on last update timestamp
 # * Permit & Legislation specific filters
 class GeoViewer < ActiveRecord::Base
-  
+
   acts_as_content_node({
     :allowed_roles_for_update          => %w( admin final_editor ),
     :allowed_roles_for_create          => %w( admin ),
     :allowed_roles_for_destroy         => %w( admin ),
     :available_content_representations => ['content_box']
   })
-  
+
   validates_presence_of :title
   validates_length_of   :title, :in => 2..255, :allow_blank => true
-  
+
   serialize :filter_settings
   serialize :map_settings
-  
+
   def tree_icon_class
     'map_icon'
   end
-  
+
   def icon_filename
     'map.png'
   end
-  
+
   def filter_settings
     read_attribute(:filter_settings) || {}
   end
-  
+
   def map_settings
     read_attribute(:map_settings) || {}
   end
-  
+
   def nodes(filters = {})
-    if !filters.nil?
+    if filters.present?
       filters[:search_scope] ||= self.filter_settings[:search_scope]
       filters[:search_scope] ||= 'all'
     else
       filters = []
       filters[:search_scope] ||= 'all'
     end
-    
+
     nodes = if filters[:search_scope] =~ /node_(\d+)/
       Node.find($1).self_and_descendants
     elsif filters[:search_scope] =~ /content_type_(\w+)/
       Node.scoped(:conditions => { :content_type => $1.classify })
-    elsif filters[:search_scope] == 'all'
+    else
       Node.scoped({})
     end
-    
+
     if filters[:from_date].present?
       nodes = nodes.published_after((Time.parse(filters[:from_date]) rescue nil))
     end
@@ -85,20 +85,16 @@ class GeoViewer < ActiveRecord::Base
       nodes = nodes.scoped(:joins => 'LEFT JOIN permits on permits.id = nodes.content_id AND nodes.content_type = \'Permit\'')
       nodes = nodes.scoped(:conditions => { :permits => { :phase_id        => filters[:permit_phase]       }}) if filters[:permit_phase].present?
       nodes = nodes.scoped(:conditions => { :permits => { :product_type_id => filters[:permit_product_type]}}) if filters[:permit_product_type].present?
-    end
-
-    if filters[:search_scope] == 'content_type_legislation'
+    elsif filters[:search_scope] == 'content_type_legislation'
       #Permit filters
       nodes = nodes.scoped(:joins => 'LEFT JOIN legislations on legislations.id = nodes.content_id AND nodes.content_type = \'Legislation\'')
       nodes = nodes.scoped(:conditions => { :legislations => { :subject => filters[:legislation_subject] }}) if filters[:legislation_subject].present?
     end
 
-    if !nodes.nil?
+    if nodes.present?
       return nodes.geo_coded.find_accessible(:all) 
     else
       return nil
     end
-
-    return nodes.geo_coded.find_accessible(:all)
   end
 end
