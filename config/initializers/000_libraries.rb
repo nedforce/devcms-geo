26713cd59b2751929a10b8a18a5a1e3b6eafe174
Gerjan Stokkink <gerjan.stokkink@gmail.com>
2011-12-08 12:31:50 +0100

    Bugfix.

--- config/initializers/000_libraries.rb
+++ config/initializers/000_libraries.rb
@@ -1,6 +1,8 @@
 require 'acts_as_content_node_geo_extensions'
 require 'static_map'
 
-Node.send(:include, Node::GeoLocation)
+if ActiveRecord::Base.connection.table_exists?('nodes')
+  Node.send(:include, Node::GeoLocation)
+end
 
 ActiveRecord::Base.send(:include, Acts::ContentNode::GeoExtensions)
