246cf7730fe1f8e78253ecb73bcc9f6b7a53ef9c
Gerjan Stokkink <gerjan.stokkink@gmail.com>
2012-01-06 17:24:37 +0100

    [ #7282: done. Added paranoid deletion functionality. ]

--- config/initializers/000_libraries.rb
+++ config/initializers/000_libraries.rb
@@ -1,8 +1,5 @@
-require 'acts_as_content_node_geo_extensions'
 require 'static_map'
 
 if ActiveRecord::Base.connection.table_exists?('nodes')
   Node.send(:include, Node::GeoLocation)
 end
-
-ActiveRecord::Base.send(:include, Acts::ContentNode::GeoExtensions)
