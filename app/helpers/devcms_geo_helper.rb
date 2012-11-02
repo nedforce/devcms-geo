537bc23536fb7f4997f24a157593bad96eccfc56
Roel van der Hoorn <vanderhoorn@gmail.com>
2012-11-02 14:50:16 +0100

    Improve the alt attribute of Google maps.

--- app/helpers/devcms_geo_helper.rb
+++ app/helpers/devcms_geo_helper.rb
@@ -1,16 +1,18 @@
 module DevcmsGeoHelper
-  
+
   def default_fields_after_form(form)
-    after_form_fields = super    
-    geo_data_block = render '/admin/geo_viewers/geo_data_fields', :form => form    
+    after_form_fields = super
+    geo_data_block = render '/admin/geo_viewers/geo_data_fields', :form => form
     raw [after_form_fields, geo_data_block].join
-  end  
+  end
 
   # Given an array of addresses, will return an image an
-  # image tag with a static google map plotting those points.
-  def static_map_of_addresses(addresses, options = {})
+  # image tag with a static Google map plotting those points.
+  def static_map_of_addresses(addresses, options = {}, alt)
     if !addresses.nil?
-      image_tag(DevcmsGeo::StaticMap.for_addresses(addresses, options), :alt => "A map of #{pluralize addresses.size, "address"}")
+      alt = "A map of #{pluralize addresses.size, "address"}" if !alt
+
+      image_tag(DevcmsGeo::StaticMap.for_addresses(addresses, options), :alt => alt)
     end
   end
 end
