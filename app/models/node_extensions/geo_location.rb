4476c432adbff711359bd709fa40cfe769273d72
Roel van der Hoorn <vanderhoorn@gmail.com>
2013-04-04 16:56:42 +0200

    Separate the geocoder from the geocode method, so we can reuse it in other models as well.

--- app/models/node_extensions/geo_location.rb
+++ app/models/node_extensions/geo_location.rb
@@ -5,18 +5,18 @@ module NodeExtensions::GeoLocation
     acts_as_mappable :default_units => :kms
 
     belongs_to :pin
-    
+
     before_validation :geocode_if_location_changed
 
     validates_presence_of :lng, :lat, :if => :location_present_and_valid?
     validate :valid_location
 
     scope :geo_coded, { :conditions => 'nodes.lat IS NOT NULL AND nodes.lng IS NOT NULL' }
-  
+
     scope :published_after, lambda { |date| {
       :conditions => ["(publication_start_date >= DATE(:date) OR publication_start_date IS NULL) AND (publication_end_date >= DATE(:date) OR publication_end_date IS NULL)", { :date => date }] }
     }
-  
+
     scope :published_before, lambda { |date| {
       :conditions => ["(publication_end_date <= DATE(:date) OR publication_end_date IS NULL) AND (publication_start_date <= DATE(:date) OR publication_start_date IS NULL)", { :date => date }] }
     }
@@ -24,7 +24,7 @@ module NodeExtensions::GeoLocation
     attr_accessor :location_coordinates
 
   end
-  
+
   module ClassMethods
     # Get the geocoding bias from settings, or default to NL
     def geocoding_bias
@@ -37,13 +37,21 @@ module NodeExtensions::GeoLocation
       return bias.present? ? bias : 'NL'
     end
   end  
-  
+
   def own_or_inherited_pin
     @own_or_inherited_pin ||= pin.present? ? pin : ancestors.all(:select => :pin_id, :include => :pin, :order => ['nodes.ancestry_depth desc'], :conditions => 'nodes.pin_id is not null').first.try(:pin)
   end
-    
+
   def location_present?
     self.location.present?
+  end
+
+  def self.geocode(*args)
+    begin
+      Geokit::Geocoders::GoogleGeocoder.geocode(args)
+    rescue Geokit::TooManyQueriesError
+      nil
+    end
   end
 
   private
@@ -58,11 +66,7 @@ module NodeExtensions::GeoLocation
       if self.location.is_a?(Geokit::GeoLoc)
         @geocode = self.location
       else
-        begin
-          @geocode = Geokit::Geocoders::GoogleGeocoder.geocode(self.location, :bias => Node.geocoding_bias)
-        rescue Geokit::TooManyQueriesError
-          @geocode = nil
-        end
+        @geocode = Node.geocode(self.location, :bias => Node.geocoding_bias)
       end
 
       if @geocode && @geocode.success
@@ -83,13 +87,12 @@ module NodeExtensions::GeoLocation
   def valid_location
     self.errors.add(:base, I18n.t('nodes.invalid_location')) if location_invalid?
   end
-  
+
   def location_invalid?
     @geocode.present? && !@geocode.success
   end
-  
+
   def location_present_and_valid?
     location_present? && @geocode.present? && !location_invalid?
   end
-
 end
