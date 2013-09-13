32deecacc58910dfd0f1a7a3ccdea1e6e39cf028
Robin Janse <r.janse@nedforce.nl>
2013-09-13 13:05:51 +0200

    Changed the provider to Yahoo and provided a Yahoo api key.

--- config/initializers/geokit_config.rb
+++ config/initializers/geokit_config.rb
@@ -23,7 +23,7 @@ if defined? Geokit
 	# This is your yahoo application key for the Yahoo Geocoder.
 	# See http://developer.yahoo.com/faq/index.html#appid
 	# and http://developer.yahoo.com/maps/rest/V1/geocode.html
-	Geokit::Geocoders::yahoo = 'REPLACE_WITH_YOUR_YAHOO_KEY'
+	Geokit::Geocoders::yahoo = Settler[:yahoo_maps_key] if defined?(SETTLER_LOADED) && SETTLER_LOADED
     
 	# This is your Google Maps geocoder key. 
 	# See http://www.google.com/apis/maps/signup.html
@@ -54,7 +54,7 @@ if defined? Geokit
 	# Be aware that there are Terms of Use restrictions on how you can use the 
 	# various geocoders.  Make sure you read up on relevant Terms of Use for each
 	# geocoder you are going to use.
-	Geokit::Geocoders::provider_order = [:google]
+	Geokit::Geocoders::provider_order = [:yahoo]
 
 	# The IP provider order. Valid symbols are :ip,:geo_plugin.
 	# As before, make sure you read up on relevant Terms of Use for each
