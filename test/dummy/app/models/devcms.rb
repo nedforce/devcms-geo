a1b05ff496ce25e94819321c7e613cd1d6b5cb81
Roel van der Hoorn <vanderhoorn@gmail.com>
2014-07-26 17:01:46 +0200

    Disable WeblogArchives as they are unused (for a long time) on Deventer and Olst-Wijhe.

--- test/dummy/app/models/devcms.rb
+++ test/dummy/app/models/devcms.rb
@@ -9,7 +9,7 @@ class Devcms
           :allowed_child_content_types => %w(
             AlphabeticIndex Attachment AttachmentTheme Calendar Carrousel CombinedCalendar ContactBox ContactForm Feed Forum GeoViewer
             HtmlPage Image LinksBox InternalLink ExternalLink NewsArchive NewsletterArchive NewsViewer
-            Page Poll SearchPage Section SocialMediaLinksBox WeblogArchive
+            Page Poll SearchPage Section SocialMediaLinksBox
           ),
           :allowed_roles_for_create  => %w( admin final_editor ),
           :allowed_roles_for_destroy => %w( admin final_editor ),
@@ -20,7 +20,7 @@ class Devcms
           :allowed_child_content_types => %w(
             AlphabeticIndex Attachment AttachmentTheme Calendar Carrousel CombinedCalendar ContactBox ContactForm Feed Forum GeoViewer
             HtmlPage Image LinksBox InternalLink ExternalLink NewsArchive NewsletterArchive NewsViewer
-            Page Poll SearchPage Section Site SocialMediaLinksBox WeblogArchive
+            Page Poll SearchPage Section Site SocialMediaLinksBox
           ),
           :allowed_roles_for_create  => %w( admin ),
           :allowed_roles_for_update  => %w( admin ),
