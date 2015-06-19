686be8f24e5a70eec5dd4cdafd8deb16a2278bd2
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-06-19 10:32:34 +0200

    Cleanup the geo_location model using rubocop.

--- app/models/node_extensions/geo_location.rb
+++ app/models/node_extensions/geo_location.rb
@@ -2,25 +2,25 @@ module NodeExtensions::GeoLocation
   extend ActiveSupport::Concern
 
   included do
-    acts_as_mappable :default_units => :kms
+    acts_as_mappable default_units: :kms
 
     belongs_to :pin
 
     # Geocode before validation or after, if defer_geocoding is set
-    before_validation :geocode_if_location_changed, :unless => :defer_geocoding
-    before_save       :geocode_if_location_changed,     :if => :defer_geocoding
+    before_validation :geocode_if_location_changed, unless: :defer_geocoding
+    before_save       :geocode_if_location_changed,     if: :defer_geocoding
 
-    validates_presence_of :lng, :lat, :if => :location_present_and_valid?
+    validates_presence_of :lng, :lat, if: :location_present_and_valid?
     validate :valid_location
 
-    scope :geo_coded, { :conditions => 'nodes.lat IS NOT NULL AND nodes.lng IS NOT NULL' }
+    scope :geo_coded, { conditions: 'nodes.lat IS NOT NULL AND nodes.lng IS NOT NULL' }
 
     scope :published_after, lambda { |date| {
-      :conditions => ['publication_start_date >= DATE(:date)', { :date => date }] }
+      conditions: ['publication_start_date >= DATE(:date)', { date: date }] }
     }
 
     scope :published_before, lambda { |date| {
-      :conditions => ['publication_start_date <= DATE(:date)', { :date => date }] }
+      conditions: ['publication_start_date <= DATE(:date)', { date: date }] }
     }
 
     attr_accessor :location_coordinates, :defer_geocoding
@@ -33,48 +33,47 @@ module NodeExtensions::GeoLocation
         bias = Settler[:geocode_bias]
         bias = GeoKit::Bounds.normalize(bias) rescue 'NL' if bias.present?
       end
-      return bias.present? ? bias : 'NL'
+      bias.present? ? bias : 'NL'
     end
 
     def try_geocode(*args)
-      begin
-        Geokit::Geocoders::GoogleGeocoder3.geocode(*args)
-      rescue Geokit::TooManyQueriesError
-        nil
-      end
+      Geokit::Geocoders::GoogleGeocoder3.geocode(*args)
+    rescue Geokit::TooManyQueriesError
+      nil
     end
   end
 
   def own_or_inherited_pin
-    @own_or_inherited_pin ||= pin.present? ? pin : ancestors.all(:select => :pin_id, :include => :pin, :order => ['nodes.ancestry_depth desc'], :conditions => 'nodes.pin_id is not null').first.try(:pin)
+    @own_or_inherited_pin ||= pin.present? ? pin : ancestors.all(select: :pin_id, include: :pin, order: ['nodes.ancestry_depth desc'], conditions: 'nodes.pin_id is not null').first.try(:pin)
   end
 
   def location_present?
-    self.location.present?
+    location.present?
   end
 
   private
 
-  # Geocode location into lat and lng, also set location field to full address for future reference
+  # Geocode location into lat and lng.
+  # Also set location field to full address for future reference.
   def geocode_if_location_changed
     geocode! if (location_changed? && !(lat_changed? || lng_changed?)) || location_coordinates.present?
   end
 
   def geocode!
-    if self.location.present?
-      if self.location.is_a?(Geokit::GeoLoc)
-        @geocode = self.location
+    if location.present?
+      if location.is_a?(Geokit::GeoLoc)
+        @geocode = location
       else
-        @geocode = Node.try_geocode(self.location, :bias => Node.geocoding_bias)
+        @geocode = Node.try_geocode(location, bias: Node.geocoding_bias)
       end
 
       if @geocode && @geocode.success
         self.lat = @geocode.lat
         self.lng = @geocode.lng
         self.location = @geocode.full_address
-        self.location_code = "#{@geocode.zip} #{@geocode.street_number}".gsub(/\s+/, "")
+        self.location_code = "#{@geocode.zip} #{@geocode.street_number}".gsub(/\s+/, '')
       end
-    elsif self.location_coordinates.present?
+    elsif location_coordinates.present?
       ll = Geokit::LatLng.normalize(location_coordinates)
       self.lat = ll.lat
       self.lng = ll.lng
@@ -85,7 +84,7 @@ module NodeExtensions::GeoLocation
   end
 
   def valid_location
-    self.errors.add(:base, I18n.t('nodes.invalid_location')) if location_invalid?
+    errors.add(:base, I18n.t('nodes.invalid_location')) if location_invalid?
   end
 
   def location_invalid?
