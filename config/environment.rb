65d9b434e2378da8852a7f2da9852b563000032b
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-09-18 17:04:12 +0200

    Allow higher versions than 1.5 of geokit.

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ config.plugin_paths   += Dir["#{File.dirname(__FILE__)}/../vendor/plugins"]
 config.autoload_paths += Dir["#{File.dirname(__FILE__)}/../app/uploaders"]
 config.i18n.load_path += Dir[File.join(File.dirname(__FILE__), 'locales', '**', '*.{rb,yml}')]
 
-config.gem 'geokit', :version => '~> 1.5.0'
+config.gem 'geokit', :version => '~> 1.5'
 config.gem 'carrierwave', :version => '~> 0.4.10'
