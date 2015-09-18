c73da8f7df80012566945939ed03a618e506988f
Roel van der Hoorn <vanderhoorn@gmail.com>
2015-09-18 16:43:12 +0200

    Fix deploy

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
-# Geo environement.rb
+# Geo environment.rb
 config.plugin_paths   += Dir["#{File.dirname(__FILE__)}/../vendor/plugins"]
-config.autoload_paths   += Dir["#{File.dirname(__FILE__)}/../app/uploaders"]
+config.autoload_paths += Dir["#{File.dirname(__FILE__)}/../app/uploaders"]
 config.i18n.load_path += Dir[File.join(File.dirname(__FILE__), 'locales', '**', '*.{rb,yml}')]
 
-config.gem 'geokit', :version => '~> 1.6.7'
+config.gem 'geokit'
 config.gem 'carrierwave', :version => '~> 0.4.10'
