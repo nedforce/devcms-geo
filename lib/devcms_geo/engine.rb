2041326f02dd8c88c9e32927f42a8fca49aab0a4
Martijn Bolhuis <martijn.bolhuis@gmail.com>
2016-05-18 14:20:33 +0200

    Use new devcms-core functionality to show geo_viewer fields in the node form

--- lib/devcms_geo/engine.rb
+++ lib/devcms_geo/engine.rb
@@ -5,7 +5,7 @@ module DevcmsGeo
     ActiveSupport.on_load(:node) do
       include NodeExtensions::GeoLocation
     end
-
+    DevcmsCore.config.node_field_partials << 'admin/geo_viewers/node_fields'
     initializer 'devcms_precompile' do |app|
       app.config.assets.precompile << 'oms.js'
       app.config.assets.precompile << 'devcms-geo.js'
